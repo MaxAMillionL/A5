9a1bbc9a2083dbabcc7c5c3d5b1247e92a5418ad
Venus Dinari <vd4689@princeton.edu>
2025-04-10 19:04:14 -0400

    flat done

--- mywcflat.c
+++ mywcflat.c
@@ -62,43 +62,48 @@ int main(void)
 /*this is theflattened c im just using the top code as reference*/
 
 
+
+
+
 int main(void)
-{
-   while ((iChar = getchar()) != EOF)
-   {
+
+loop1:
+   if ((iChar = getchar()) = EOF) goto endloop1;
+   
       lCharCount++;
 /*if a space is detected we either evaluate if we shoudl chnage the inword boolean to false after we increase the word count or if we are in a word set the boolean to true. 
 
 */ 
-      if (!(isspace(iChar))) goto spaceEnd;
-      {
+      if (!(isspace(iChar))) goto else1;
+
     /*if the inword boolean was previously set to true as in we were ina  word then increase teh word count and then set inwrod to false since we detected a space as the car*/
-         if (!iInWord) goto else1;
+         if (!iInWord) goto endinWord;
          {
             lWordCount++;
             iInWord = FALSE;
-            goto endif1;
-
+            
+endinWord:
 else1:
  /*if the inword boolean was previously set to FALSE and a new charcter that isnta spcae or a newline is detected then we are in a new word so set the inword boolean to true.*/
-     if (iInWord) goto endinWord;
+     if (iInWord) goto endnotinWord;
             iInWord = TRUE;
            
-endinWord:
-endif1:
-      }
+endnotinWord:
+
 /*increase the newline count if the new line charcter is detected*/
 
       if (!(iChar == '\n')) goto newlineEnd;
          lLineCount++;
 newlineEnd:
-   
+
+    goto loop1;
+endloop1:
 /*this is for the last word if you dont add a space after 
 the last word */
    if (!iInWord) goto wordEnd;
       lWordCount++;
-      
 wordEnd:
+
 
    printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
    return 0;
